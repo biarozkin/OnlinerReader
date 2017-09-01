c7a6dc2d3a59222c8935082985cca59e5278c6c6
biarozkin <biarozkin@gmail.com>
2017-09-01 13:36:19 +0300

    Initial commit. Let's begin!

---
+++ OnlinerReader/NewsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsViewController.swift
+//  OnlinerReader
+//
+//  Created by Igor Biarozkin on 01.09.17.
+//  Copyright © 2017 biarozkin. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
